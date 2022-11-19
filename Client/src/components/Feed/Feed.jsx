7e1b196f86c5ed505d2d579759b37aa469adcf53
Andrew Dye <phodye@gmail.com>
2022-11-19 15:05:22 -0500

    Updated basic file structure for app components

---
+++ Client/src/components/Feed/Feed.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const { useState, useEffect } = React;
+
+function Feed () {
+  return (<div className="border-2 border-red-900 m-5 p-1">
+    <h2>
+      FEED
+    </h2>
+  </div>)
+}
+
+export default Feed;
---
+++ Client/src/components/Profile/Bio.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const { useState, useEffect } = React;
+
+function Bio () {
+  return (<div>
+    <h3 className="italic">
+      Bio
+    </h3>
+  </div>)
+}
+
+export default Bio;
