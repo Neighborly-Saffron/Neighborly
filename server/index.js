4ea95b343a77454420882e907c35c107b1bb6b4e
Gabe Yamartino <gabeyamartino@gmail.com>
2022-11-19 16:47:42 -0500

    deleted bundle.js

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 
-const port = 3000
+const port = 3001
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
