475e1fa1a79c975011cf6f364520a7ff47295a49
Gabe Yamartino <gabeyamartino@gmail.com>
2022-11-20 12:03:06 -0500

    Created login, sign up, and logout buttons that communicate with auth0 and are working well

---
+++ Client/src/components/Login/LogoutButton.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+
+const LogoutButton = () => {
+  const { logout } = useAuth0();
+  return (
+    <button
+      className="border-2 border-cyan-800 m-5 p-1"
+      onClick={() =>
+        logout({
+          returnTo: window.location.origin,
+        })
+      }
+    >
+      Log Out
+    </button>
+  );
+};
+
+export default LogoutButton;
