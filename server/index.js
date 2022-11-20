524b1914ad5cf263654591b5027ba81306d99cab
Huong <htnguuyen@gmail.com>
2022-11-20 13:37:25 -0500

    required controller & added express.static

--- server/index.js
+++ server/index.js
@@ -1,6 +1,15 @@
+const groups = require('../controllers/group/groups.js')
+const path = require("path");
+
 const express = require('express')
 const app = express()
 app.use(express.json())
+
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/usergroups/:userId', groups.getUserGroups);
+
 
 const port = 3000
 
