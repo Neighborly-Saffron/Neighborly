aecf5999ddd69c6672e9dd06cf7f7a4b100cdb2d
Andrew Dye <phodye@gmail.com>
2022-11-20 20:07:14 -0500

    Added post component, initial query for home feed and like functionality

--- Client/src/components/Feed/Post.jsx
+++ Client/src/components/Feed/Post.jsx
@@ -1,23 +1,36 @@
 import React from 'react';
+import axios from 'axios';
 
 const { useState, useEffect } = React;
 
-function Post() {
+function Post({ postData }) {
+  const [likes, setLikes] = useState(postData.likes)
+  const [hasLiked, setHasLiked] = useState(false)
+
+  const likePost = (postid) => {
+    if (!hasLiked) {
+    axios.put(`/posts`, {postid})
+      .then((res) => {
+          console.log('liked');
+          setLikes(likes + 1)
+          setHasLiked(!hasLiked)
+        })
+        .catch((err) => console.log('error liking post'))
+      }
+  }
+
   return (
     <div className="border-2 m-2 border-black bg-blue-700">
       <div className="flex">
-        <div className="aspect-square h-20 w-20 border-2 border-black text-xs text-center m-1 bg-white">img</div>
+        <img className='object-scale-down h-20 w-20 m-1' src={postData.pictureurl} alt={postData.name}></img>
         <div className="flex flex-col p-3">
-          <h3 className="italic text-white">Group Name</h3>
-          <h3 className="italic text-red-500">User Name</h3>
-          <p className="text-white">Dummy post text. Some more dummy post text. Lots of dummy post text.Dummy post text. Some more dummy post text. Lots of dummy post text.Dummy post text. Some more dummy post text. Lots of dummy post text.Dummy post text. Some more dummy post text. Lots of dummy post text. Dummy post text. Some more dummy post text. Lots of dummy post text.Dummy post text. Some more dummy post text. Lots of dummy post text.Dummy post text. Some more dummy post text. Lots of dummy post text.Dummy post text. Some more dummy post text. Lots of dummy post text.</p>
+          <h3 className="italic text-white">{postData.groupname}</h3>
+          <h3 className="italic text-red-500">{postData.username}</h3>
+          <p className="text-white">{postData.message}</p>
         </div>
       </div>
       <div className="flex justify-end">
-        <div className="h-6 w-6 border-2 border-black m-1 bg-red-500"></div>
-        <div className="h-6 w-6 border-2 border-black m-1 bg-red-500"></div>
-        <div className="h-6 w-6 border-2 border-black m-1 bg-red-500"></div>
-        <div className="h-6 w-6 border-2 border-black m-1 bg-red-500"></div>
+        <div onClick={() => {likePost(postData.postid)}} className="h-6 w-6 border-2 border-black m-1 bg-red-500 text-white rounded-full text-center">{likes}</div>
       </div>
     </div>
   )
--- server/index.js
+++ server/index.js
@@ -1,15 +1,18 @@
 const groups = require('../controllers/group/groups.js')
+const feed = require('../controllers/feed/feed.js')
 const path = require("path");
 
 const express = require('express')
 const app = express()
 app.use(express.json())
 
-
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/usergroups/:userId', groups.getUserGroups);
 
+//feed routes
+app.get('/posts', feed.getHomeFeed);
+app.put('/posts', feed.likePost);
 
 const port = 3000
 
