d99ca71d628a4d25a2cabbf22e0a16dc441cc190
Gabe Yamartino <gabeyamartino@gmail.com>
2022-11-21 14:55:50 -0500

    established working login/signup with savings username

--- Client/src/components/Login/SignUpButton.jsx
+++ Client/src/components/Login/SignUpButton.jsx
@@ -3,12 +3,13 @@ import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const SignupButton = () => {
-  const { loginWithPopup } = useAuth0();
+  const { loginWithPopup, user, user_metadata } = useAuth0();
+  console.log(user)
   return (
     <button
       className="border-2 border-cyan-800 m-5 p-1"
       onClick={() =>
-        loginWithRedirect({
+        loginWithPopup({
           screen_hint: 'signup',
         })
       }
