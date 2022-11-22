91289a8f7eea14934778a75f265e856342a0d105
Gabe Yamartino <gabeyamartino@gmail.com>
2022-11-21 19:26:46 -0500

    Added an add new user controller

--- controllers/user/user.js
+++ controllers/user/user.js
@@ -3,7 +3,12 @@ const connectionPool = require('../../db/pool.js')
 const addNewUser = (req, res) => {
 
   let query = `INSERT into users (authId, name, bio, pictureUrl)
-  SELECT 'andrew dye', 'andrew dye'
-  WHERE NOT EXISTS (SELECT id from users WHERE name = 'andrew dye')
-  RETURNING id;`
+  SELECT '${req.body.authId}', '${req.body.name}', '${req.body.bio}', '${req.body.pictureUrl}'
+  WHERE NOT EXISTS (SELECT authId FROM users WHERE authId = '${req.body.authId}');`
+
+  connectionPool.query(query)
+    .then((data) => res.status(200).end())
+    .catch((err) => console.log(err))
 }
+
+module.exports = { addNewUser }
