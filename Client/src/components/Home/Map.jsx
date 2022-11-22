10fd565bf0f103d6e3996782566084743ac34ec7
Andrew Fuller <fuller2580@gmail.com>
2022-11-21 19:04:43 -0500

    dynamic map

--- Client/src/components/Home/Map.jsx
+++ Client/src/components/Home/Map.jsx
@@ -3,20 +3,19 @@ import GoogleMap from 'google-map-react'
 import Marker from './Marker.jsx'
 const { useState, useEffect } = React;
 
-function Map () {
+function Map (props) {
 
-  let [mapLng, setMapLng] = useState(-73.98546651170592)
-  let [mapLat, setMapLat] = useState(40.757901544177926)
-  let events = [{name: 'event1', lat:40.65, lng:-73.98}, {name: 'event2', lat:40.65, lng:-74.4}]
+  let [mapLng, setMapLng] = useState()
+  let [mapLat, setMapLat] = useState()
+
   return (
   <div className="map" style={{width:'30vw', height:'30vh'}}>
       <GoogleMap
         apiKey={[process.env.googleAPI]}
-        center= {[mapLat, mapLng]}
+        center= {props.mapStart.latlng}
         zoom= {9}>
-          {events.map((event)=> {
+          {props.eventList.events.map((event)=> {
             return <Marker name={event.name} lat={event.lat} lng={event.lng}></Marker>
-
           })}
       </GoogleMap>
   </div>
