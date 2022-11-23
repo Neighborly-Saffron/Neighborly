da9f176652d7f584d8588bb14c45aa15ae021d63
Andrew Fuller <fuller2580@gmail.com>
2022-11-22 21:25:17 -0500

    group page map loading

--- Client/src/components/Group/GroupEventMap.jsx
+++ Client/src/components/Group/GroupEventMap.jsx
@@ -1,13 +1,33 @@
 import React from 'react';
+import Map from '../Home/Map.jsx';
+import axios from 'axios';
 
 const { useState, useEffect } = React;
 
 function GroupEventMap () {
+const [eventList, setEvents] = useState({events:[]})
+const [mapStart, setMapStart] = useState({latlng:[]})
+const getEvents = () => {
+  axios.get(`/mapEvents`)
+    .then((res) => {
+      setEvents({events:res.data});
+      console.log('mapEvents', res.data);
+    })
+    .catch((err) => console.log('error getting group event data'))
+}
+useEffect(()=>{
+  if(eventList.events.length) {
+    setMapStart({latlng:[eventList.events[0].json_build_object.lat, eventList.events[0].json_build_object.lng]})
+  }
+},[eventList])
+
+useEffect(()=>{
+  console.log(mapStart);
+},[mapStart])
+useEffect(getEvents, [])
   return (
   <>
-    <h3 className="italic">
-      Group Event Map
-    </h3>
+    <Map mapStart={mapStart}eventList={eventList}/>
   </>
   )
 }
