bbc6a65a388329a0db7635ae61e8897d17f41657
Parker Sturtevant <wsturtevant196@gmail.com>
2022-11-26 12:38:18 -0500

    Trying to fix admin group

--- Client/src/components/Modals/RequestList.jsx
+++ Client/src/components/Modals/RequestList.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import Request from './Request.jsx'
-import {useEffect, useState} from 'react'
+import Request from './Request.jsx';
+import { useEffect, useState } from 'react';
 
+const RequestList = ({
+	group,
+	requestedUsers,
+	handleNotificationClick,
+	decreaseNotificationCount,
+	getRequestedGroups,
+}) => {
+	console.log('it got in requestedlist');
+	const [displayRequests, setDisplayRequests] = useState([]);
 
-const RequestList = ({group, requestedUsers, handleNotificationClick, setNotificationCount}) => {
-  console.log('it got in requestedlist')
-  const [displayRequests, setDisplayRequests] = useState([])
-
-  const filter = () => {
+	const filter = () => {
 		const filteredRequests = [];
 		requestedUsers.forEach((requestObj) => {
 			if (requestObj.groupName === group.groupName) {
@@ -17,37 +22,48 @@ const RequestList = ({group, requestedUsers, handleNotificationClick, setNotific
 		setDisplayRequests(filteredRequests);
 	};
 
-  useEffect(()=>{
-    filter()
-  }, [])
+	useEffect(() => {
+		filter();
+	}, []);
 
-  return (
-    <>
-
-<div className="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
-
-
-<div className="fixed inset-0 z-40 overflow-y-auto h-fit w-fit m-auto">
-  <div className="flex min-h-fit items-end justify-center p-4 z-50 text-center sm:items-center sm:p-0">
-    <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg z-50">
-      <div className="bg-white z-50 px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-      <div className="flex z-50 items-center h-fit w-96">
-      <div className="w-full border-2 border-black bg-lighterblue rounded shadow-lg p-8 m-1">
-          {displayRequests.length !== 0 && displayRequests.map((requestedUser, index)=>{
-          return <Request key={index} requestedUser={requestedUser} displayRequests={displayRequests}  handleNotificationClick={handleNotificationClick}></Request>
-          })
-          }
-      </div>
-    </div>
-      </div>
-    </div>
-  </div>
-</div>
-</div>
-
-</>
-  )
-}
+	return (
+		<>
+			<div
+				className="relative z-10"
+				aria-labelledby="modal-title"
+				role="dialog"
+				aria-modal="true"
+			>
+				<div className="fixed inset-0 z-40 overflow-y-auto h-fit w-fit m-auto">
+					<div className="flex min-h-fit items-end justify-center p-4 z-50 text-center sm:items-center sm:p-0">
+						<div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg z-50">
+							<div className="bg-white z-50 px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+								<div className="flex z-50 items-center h-fit w-96">
+									<div className="w-full border-2 border-black bg-lighterblue rounded shadow-lg p-8 m-1 flex flex-col gap-3">
+										{displayRequests.length !== 0 &&
+											displayRequests.map((requestedUser, index) => {
+												return (
+													<Request
+														key={index}
+														requestedUser={requestedUser}
+														displayRequests={displayRequests}
+														handleNotificationClick={handleNotificationClick}
+														decreaseNotificationCount={
+															decreaseNotificationCount
+														}
+                            getRequestedGroups={getRequestedGroups}
+													></Request>
+												);
+											})}
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
 
 export default RequestList;
-
