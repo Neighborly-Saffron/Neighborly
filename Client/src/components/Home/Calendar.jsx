a2e86892ef0b883136b6d9e4c2b59425cb5ed4ee
Andrew Fuller <fuller2580@gmail.com>
2022-11-26 18:56:50 -0500

    days with events show green on calendar

--- Client/src/components/Home/Calendar.jsx
+++ Client/src/components/Home/Calendar.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import Calendar from 'react-calendar'
-import 'react-calendar/dist/Calendar.css'
+import '../../../Assets/reactCalendar.css'
 import EventList from '../Group/GroupEventList.jsx'
 
 const { useState, useEffect } = React;
 const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
 ];
+
+const today = new Date();
+today.setDate(today.getDate()+1)
+const tomorrow = new Date(today);
+tomorrow.setDate(tomorrow.getDate() + 3)
+const datesToAddClassTo = [today, tomorrow];
+
 
 function CalendarComponent (props) {
   const [eventList, setEventList] = useState({events:[]});
@@ -19,31 +26,53 @@ function CalendarComponent (props) {
   },[date])
   useEffect(()=> {
     checkEvents();
+    tileClassName({date:date,view:'month'});
   },[props.eventList])
   let checkEvents = () => {
     var dateEvents = []
     props.eventList.events.forEach(event =>{
       let eventDate = new Date(event.json_build_object.date);
-      if(
-        date.getDate() === eventDate.getDate() &&
-        date.getMonth() === eventDate.getMonth() &&
-        date.getFullYear() === eventDate.getFullYear()
-      ) {
-        dateEvents.push(event);
+      if(checkDate(date, eventDate)) {
+          dateEvents.push(event);
+        }
+      })
+      setEventList({events: dateEvents})
+    }
+    function tileClassName({ date, view }) {
+      // Add class to tiles in month view only
+      if (view === 'month') {
+        // Check if a date React-Calendar wants to check is on the list of dates to add class to
+        if (props.eventList.events.find(event => {
+          let eventDate = new Date(event.json_build_object.date);
+          return checkDate(date, eventDate);
+          }))
+          {
+            return 'hasEvent';
+          }
       }
+    }
+    let checkDate = ((checkDate, eventDate) => {
+      if(checkDate.getDate() === eventDate.getDate() &&
+        checkDate.getMonth() === eventDate.getMonth() &&
+        checkDate.getFullYear() === eventDate.getFullYear()
+      ) {return true}
+      return false
     })
-    setEventList({events: dateEvents})
-  }
 
-  return (
-  <div>
+    return (
+      <div className='flex gap-2 sm:flex-col'>
       <Calendar
         onChange={onChange}
         value= {date}
-      >
+        tileClassName={tileClassName}
+        calendarType={'US'}
+        >
       </Calendar>
-      <h3 className='italic'> Events for {monthNames[date.getMonth()]} {date.getDate()}, {date.getFullYear()}</h3>
-      <EventList eventList={eventList}></EventList>
+      <div>
+        <h3 className='italic'> Events for {monthNames[date.getMonth()]} {date.getDate()}, {date.getFullYear()}</h3>
+        <EventList eventList={eventList}></EventList>
+
+      </div>
   </div>
   )
 }
