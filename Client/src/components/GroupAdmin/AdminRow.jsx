e4c949dee8887415b1427e73e2c1627c09e560b8
Andrew Dye <phodye@gmail.com>
2022-11-26 17:25:47 -0500

    Adjusted the admin row so the photo/group name is only responsive if there are requests and the modal is openable

--- Client/src/components/GroupAdmin/AdminRow.jsx
+++ Client/src/components/GroupAdmin/AdminRow.jsx
@@ -15,6 +15,7 @@ function AdminRow({ group, getAdminGroups }) {
   return (
     <>
     <tr>
+      {group.requests ? <>
       <th className="cursor-default hover:cursor-pointer" onClick={()=>{
          if (group.requests) {
           setToggleModal(!toggleModal)
@@ -23,12 +24,31 @@ function AdminRow({ group, getAdminGroups }) {
         src={group.groupPic}
         className="h-full w-full max-w-full object-cover rounded-lg"
       /></th>
+      </>:<>
+      <th className="cursor-default" onClick={()=>{
+        if (group.requests) {
+         setToggleModal(!toggleModal)
+       }
+     }}><img
+       src={group.groupPic}
+       className="h-full w-full max-w-full object-cover rounded-lg"
+     /></th>
+      </>}
+      {group.requests ? <>
       <th className="cursor-default hover:text-darkerblue hover:cursor-pointer" onClick={()=>{
         if (group.requests) {
           setToggleModal(!toggleModal)
         }
         }}>{group.groupName}</th>
       <th className="cursor-default">{group.requests}</th>
+      </>:<>
+      <th className="cursor-default" onClick={()=>{
+        if (group.requests) {
+          setToggleModal(!toggleModal)
+        }
+        }}>{group.groupName}</th>
+      <th className="cursor-default">{group.requests}</th>
+      </>}
     </tr>
     {toggleModal ? <AdminGroupModal switchModal={switchModal} groupId={group.groupid}/> : null }
     </>
