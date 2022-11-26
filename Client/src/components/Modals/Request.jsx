18fac1d857b02f37a9f2af1242759413558c2cee
Parker Sturtevant <wsturtevant196@gmail.com>
2022-11-26 11:23:36 -0500

    Fixed group admin

--- Client/src/components/Modals/Request.jsx
+++ Client/src/components/Modals/Request.jsx
@@ -1,60 +1,77 @@
 import React from 'react';
-import axios from 'axios'
-const Request = ({group, requestedUser, handleNotificationClick, filteredRequests, displayRequests, setDisplayRequests}) => {
-  console.log('requestedUser:', requestedUser)
-  const newUserGroup = {
-    userId: requestedUser.requestedUserId,
-    groupId: requestedUser.requestedGroupId
-  }
+import axios from 'axios';
+const Request = ({
+	requestedUser,
+	handleNotificationClick,
+	displayRequests,
+}) => {
+	const newUserGroup = {
+		userId: requestedUser.requestedUserId,
+		groupId: requestedUser.requestedGroupId,
+	};
 
-  const handleApprove = (e) => {
-    console.log('it got in handleApprove')
-    axios.post('/groupApproved', newUserGroup)
-     .then((result)=>{
-      console.log('client side: succeeded to post to db')
-     //remove modal
-     handleLastRequest()
-     return handleDeleteRequest()
-     })
-     .catch((err)=>{
-      console.log('client side: failed to post to db', err)
-     })
-  }
+	const handleApprove = (e) => {
+		console.log('it got in handleApprove');
+		axios
+			.post('/groupApproved', newUserGroup)
+			.then((result) => {
+				console.log('client side: succeeded to post to db');
+				//remove modal
+				handleLastRequest();
+				return handleDeleteRequest();
+			})
+			.catch((err) => {
+				console.log('client side: failed to post to db', err);
+			});
+	};
 
-  const handleLastRequest = () =>{
-    if (displayRequests.length === 1) {
-    handleDeleteRequest() //delete request from requestjoin table
-   return handleNotificationClick()//show and close modal
-    // const copyRequests = displayRequests.slice()
-    // console.log('copyRequests:', copyRequests)
-  }
+	const handleLastRequest = () => {
+		if (displayRequests.length === 1) {
+			handleDeleteRequest(); //delete request from requestjoin table
+			return handleNotificationClick(); //show and close modal
+		}
+	};
 
-  const handleDeleteRequest = () => {
-    return axios.delete('/groupApproved',   { params: newUserGroup } )
-      .then(()=>{
-        console.log('client side: delete data from db successfully ')
-      })
-      .catch((err)=>{
-        console.log('client side: error deleting requestjoin table')
-      })
-  }
+	const handleDeleteRequest = () => {
+		return axios
+			.delete('/groupApproved', { params: newUserGroup })
+			.then(() => {
+				console.log('client side: delete data from db successfully ');
+			})
+			.catch((err) => {
+				console.log('client side: error deleting requestjoin table');
+			});
+	};
 
-  return (
-    <div>
-
-            <div className="text-lg font-medium leading-6 text-gray-900" id="modal-title">{requestedUser.userName} has requested to join</div>
-            <div className="mt-2">
-            </div>
-            <button type="button" className="inline-flex w-full justify-center rounded-md border border-transparent bg-lightergreen px-4 py-2 text-base
+	return (
+		<div>
+			<div
+				className="text-lg font-medium leading-6 text-gray-900"
+				id="modal-title"
+			>
+				{requestedUser.userName} has requested to join
+			</div>
+			<div className="mt-2"></div>
+			<button
+				type="button"
+				className="inline-flex w-full justify-center rounded-md border border-transparent bg-lightergreen px-4 py-2 text-base
             font-medium text-white shadow-sm hover:bg-darkerblue focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto
-             sm:text-sm" onClick={handleApprove}>Approve</button>
-            <button type="button" className="inline-flex w-full justify-center rounded-md border border-transparent bg-darkergreen px-4 py-2 text-base
+             sm:text-sm"
+				onClick={handleApprove}
+			>
+				Approve
+			</button>
+			<button
+				type="button"
+				className="inline-flex w-full justify-center rounded-md border border-transparent bg-darkergreen px-4 py-2 text-base
             font-medium text-white shadow-sm hover:bg-darkerblue focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto
-             sm:text-sm" onClick={handleLastRequest}>Decline</button>
-    {/* //no need to do anything if declined */}
-</div>
-  )
-}
-}
+             sm:text-sm"
+				onClick={handleLastRequest}
+			>
+				Decline
+			</button>
+		</div>
+	);
+};
 
 export default Request;
--- Client/src/components/Modals/RequestList.jsx
+++ Client/src/components/Modals/RequestList.jsx
@@ -7,19 +7,15 @@ const RequestList = ({group, requestedUsers, handleNotificationClick, setNotific
   console.log('it got in requestedlist')
   const [displayRequests, setDisplayRequests] = useState([])
 
-  console.log('requestedUsers:', requestedUsers)
-  const filteredRequests = []
   const filter = () => {
-    requestedUsers.forEach(requestObj=>{
-      if (requestObj.groupName === group.groupName){
-
-        filteredRequests.push(requestObj)
-      }
-      console.log('filteredRequests', filteredRequests)
-
-    })
-    setDisplayRequests(filteredRequests)
-  }
+		const filteredRequests = [];
+		requestedUsers.forEach((requestObj) => {
+			if (requestObj.groupName === group.groupName) {
+				filteredRequests.push(requestObj);
+			}
+		});
+		setDisplayRequests(filteredRequests);
+	};
 
   useEffect(()=>{
     filter()
@@ -37,13 +33,10 @@ const RequestList = ({group, requestedUsers, handleNotificationClick, setNotific
       <div className="bg-white z-50 px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
       <div className="flex z-50 items-center h-fit w-96">
       <div className="w-full border-2 border-black bg-lighterblue rounded shadow-lg p-8 m-1">
-        {/* <h1 className="block w-full text-center text-2xl mx-auto p-4">Requests</h1> */}
-        {/* {requestedUsers.map((requestedUser, index)=>{ */}
-          {displayRequests.map((requestedUser, index)=>{
-          return <Request key={index} requestedUser={requestedUser} displayRequests={displayRequests} setgroup={group} handleNotificationClick={handleNotificationClick}></Request>
+          {displayRequests.length !== 0 && displayRequests.map((requestedUser, index)=>{
+          return <Request key={index} requestedUser={requestedUser} displayRequests={displayRequests}  handleNotificationClick={handleNotificationClick}></Request>
           })
           }
-
       </div>
     </div>
       </div>
