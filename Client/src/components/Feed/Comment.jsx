626641f90e8225a0b5bc4de4fb48c5cdf17e5292
Andrew Fuller <fuller2580@gmail.com>
2022-11-27 17:53:49 -0500

    time + Z

--- Client/src/components/Feed/Comment.jsx
+++ Client/src/components/Feed/Comment.jsx
@@ -12,7 +12,7 @@ function Comment({ commentData }) {
         <img className='object-scale-down h-10 w-10 m-1 rounded' src={commentData.pictureurl} alt={commentData.username}></img>
         <div className="flex flex-col p-3">
           <h3 className="italic font-bold cursor-default">{commentData.username}</h3>
-          <ReactTimeAgo className="italic text-sm cursor-default" date={Date.parse(commentData.postedat)} locale="en-US"/>
+          <ReactTimeAgo className="italic text-sm cursor-default" date={Date.parse((commentData.postedat+ 'Z'))} locale="en-US"/>
           <p className="cursor-default">{commentData.message}</p>
         </div>
       </div>
