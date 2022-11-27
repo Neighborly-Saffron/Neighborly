6e98f56cd29c4bf0a6606f0c0443673dda3ff732
Huong <htnguuyen@gmail.com>
2022-11-27 00:38:56 -0500

    added resize-none property to create group post

--- Client/src/components/Group/CreateGroupPost.jsx
+++ Client/src/components/Group/CreateGroupPost.jsx
@@ -27,7 +27,7 @@ function GroupPost ({ userId, groupId, postMessage }) {
           setMessage(tempMessage);
           setTempMessage('')
           }}>
-          <textarea className='w-full rounded p-1' rows='3' type='text' placeholder='Write something...' value={tempMessage} onChange={(e) => setTempMessage(e.target.value)}></textarea>
+          <textarea className='w-full rounded p-1 resize-none' rows='3' type='text' placeholder='Write something...' value={tempMessage} onChange={(e) => setTempMessage(e.target.value)}></textarea>
           <input className='border-2 bg-darkerblue hover:bg-lighterblue hover:border-black hover:border-2 text-white rounded p-1 px-2 cursor-pointer' type='submit' value='Post'></input>
         </form>
     </div>
