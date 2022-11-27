b2bfece2bfdd4bd4bc701d2acb2eecea03eaf36b
Lei Liang <m.leiliang@gmail.com>
2022-11-27 13:04:43 -0500

    add notification for group admin

--- Client/src/components/GroupAdmin/AdminRow.jsx
+++ Client/src/components/GroupAdmin/AdminRow.jsx
@@ -19,13 +19,13 @@ function AdminRow({ group, getAdminGroups }) {
          if (group.requests) {
           setToggleModal(!toggleModal)
         }
-      }}>
+      }}><div className='relative'>
         <img
         src={group.groupPic}
-        className="h-20 w-25 ">
-    
-        </img>
+        className="h-20 w-25 rounded-full"></img>
+       <span className="top-0 left-16 absolute  w-3.5 h-3.5 bg-lightergreen border-2 border-white dark:border-gray-800 rounded-full"></span>
 
+        </div>
       {/* <span class="flex h-3 w-3 pointer-events-none"> */}
       {/* <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-lightergreen-400 opacity-75"></span>
       <span class="relative inline-flex rounded-full h-3 w-3 bg-lightergreen-500"></span>
