fca501f877d183162b39b1a57dfbe7098da08223
Parker Sturtevant <wsturtevant196@gmail.com>
2022-11-27 19:17:05 -0500

    Various style fixes

--- Client/src/components/Groups/GroupList.jsx
+++ Client/src/components/Groups/GroupList.jsx
@@ -2,18 +2,18 @@ import axios from 'axios';
 import React from 'react';
 import GroupListItem from './GroupListItem.jsx';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 const { useState, useEffect, useRef } = React;
 
-function GroupList({userId, groupIds}) {
+function GroupList({ userId, groupIds }) {
 	const [groups, setGroups] = useState([]);
 	const [query, setQuery] = useState('');
 	const handleSubmit = (e) => {
 		e.preventDefault();
 
-    if (query.length === 0) {
-      initialGroupFetch();
-    } else {
+		if (query.length === 0) {
+			initialGroupFetch();
+		} else {
 			axios
 				.post('/searchGroups', { query: query })
 				.then((res) => {
@@ -23,7 +23,7 @@ function GroupList({userId, groupIds}) {
 				.catch((err) => {
 					console.log(err);
 				});
-    }
+		}
 	};
 
 	const initialGroupFetch = () => {
@@ -43,18 +43,28 @@ function GroupList({userId, groupIds}) {
 	}, []);
 	return (
 		<div>
-			<form className="p-2 flex gap-2  justify-center items-center" onSubmit={handleSubmit}>
-				<input
-					value={query}
-					onChange={(e) => setQuery(e.target.value)}
-					type="text"
-					className="w-7/12 h-5 p-7 mb-5 border-2 border-lightergreen rounded-lg outline-none"
-					placeholder="Search for a group..."
-				/>
-			</form>
+			<div className="p-2 flex gap-2 justify-center items-center">
+				<form className="flex gap-5 w-full justify-center items-center" onSubmit={handleSubmit}>
+				<FontAwesomeIcon className="mb-5" color="#47AA51" icon={faMagnifyingGlass} transform="grow-15"/>
+					<input
+						value={query}
+						onChange={(e) => setQuery(e.target.value)}
+						type="text"
+						className="w-7/12 h-5 p-7 mb-5 border-2 border-lightergreen rounded-lg outline-none"
+						placeholder="Search for a group..."
+					/>
+				</form>
+			</div>
 			<div className="flex flex-wrap justify-center gap-4 mb-3 p-2">
 				{groups.map((group, i) => {
-					return <GroupListItem groupIds={groupIds} userId={userId} key={i} group={group.json_build_object} />;
+					return (
+						<GroupListItem
+							groupIds={groupIds}
+							userId={userId}
+							key={i}
+							group={group.json_build_object}
+						/>
+					);
 				})}
 			</div>
 		</div>
