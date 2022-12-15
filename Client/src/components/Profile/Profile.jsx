9f8dbcd0fa7a01f6cbde5ef978bdd36f6749f1c1
Huong <htnguuyen@gmail.com>
2022-12-14 22:15:01 -0500

    changed grid cols prop to match home page

--- Client/src/components/Profile/Profile.jsx
+++ Client/src/components/Profile/Profile.jsx
@@ -10,8 +10,8 @@ const { useState, useEffect } = React
 function Profile ({ userId, userGroups, setUserGroups }) {
 
   return (
-  <div className='rounded-lg m-5 mb-0 p-10 grid grid-cols-5 gap-5'>
-    <div className='col-span-1'>
+  <div className='rounded-lg m-5 mb-0 p-10 grid grid-cols-4 gap-5'>
+    <div className='col-span-1 h-full'>
         <UserGroupList userId={userId} userGroups={userGroups}/>
       </div>
       <div className='col-start-2 col-span-4 flex flex-col gap-3'>
